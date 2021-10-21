a1735493ac2f72867fa4f3e7617d5ec4190f0024
jsmith1939 <60932568+jsmith1939@users.noreply.github.com>
2021-10-21 19:46:42 -0400

    intermediate

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
-// Your code here
 ////////////////////////////////Basic Challenge///////////////////////////////
+//////////////////////////////// Your code here //////////////////////////////
 let week = [
 	'Sunday',
 	'Monday',
@@ -10,17 +10,25 @@ let week = [
 	'Saturday',
 ];
 
-let myDateObj = new Date(); // This should print the current date
-console.assert(myDateObj instanceof Date);
-console.assert(typeof myDateObj === 'object');
+function myDateObj() {
+	return new Date();
+} // This should print the current date
+console.assert(myDateObj() instanceof Date);
+console.assert(typeof myDateObj() === 'object');
 
-let timeStamp = myDateObj.getTime();
-console.assert(typeof timeStamp === 'number');
-console.assert(Object.prototype.toString.call(timeStamp) === '[object Number]');
+function timeStamp() {
+	return myDateObj().getTime();
+}
+console.assert(typeof timeStamp() === 'number');
+console.assert(
+	Object.prototype.toString.call(timeStamp()) === '[object Number]'
+);
 
-let getDay = myDateObj.getDay();
-console.assert(typeof getDay === 'number');
-console.assert(typeof week[getDay] === 'string');
+function getDay() {
+	return myDateObj().getDay();
+}
+console.assert(typeof getDay() === 'number');
+console.assert(typeof week[getDay()] === 'string');
 
 /////////////////////// Intermediate Challenge ////////////////////////////
 
@@ -30,8 +38,22 @@ function date(dateObj) {
 	let year = dateObj.getFullYear() + '';
 	year = year.slice(-2);
 	let hour = (dateObj.getHours() + 24) % 12 || 12;
-	let mins = dateObj.getMinutes();
+	let mins = ('0' + dateObj.getMinutes()).slice(-2);
 	let timeOfDay = dateObj.getHours() > 11 ? 'pm' : 'am';
 	return `${month}/${date}/${year} - ${hour}:${mins}${timeOfDay}`;
 }
-console.log(date(myDateObj));
+console.log(date(new Date()));
+
+function daysApart(date1, date2) {
+	const diffTime = Math.abs(date2 - date1);
+	const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+	return diffDays + ' days';
+}
+console.log(
+	daysApart(
+		new Date('February 3, 2003 03:00:00'),
+		new Date('March 1, 2003 14:50:00')
+	)
+);
+
+///////////////////////// Advance Challenge: /////////////////////////////////
